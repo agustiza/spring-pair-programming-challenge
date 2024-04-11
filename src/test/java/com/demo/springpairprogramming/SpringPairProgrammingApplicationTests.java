c831ec699669c0125879db19d7da4f8198281460
Agustin Izaguirre <agustin.izaguirre@geopagos.com>
2024-04-11 10:57:35 +0200

    Initial commit

---
+++ src/test/java/com/demo/springpairprogramming/SpringPairProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springpairprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPairProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
